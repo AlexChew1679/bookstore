6f22042352f45d584c51f52cde815d86211a108d
Alex Chew <geminia79@gmail.com>
2017-05-18 00:20:35 +0800

    add paperclip

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,6 @@
 class Book < ApplicationRecord
   belongs_to :user
+  has_attached_file :image
+  has_attached_file :resource
+  #it means each book will have 2 files attachment, image and resource.
 end
