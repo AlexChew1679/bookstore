0e5e94a86377bd67decbdb3aa717c717e7f46fb5
Alex Chew <geminia79@gmail.com>
2017-05-18 02:20:17 +0800

    added storage but error

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,12 @@ class Book < ApplicationRecord
   has_attached_file :image
   has_attached_file :resource
   #it means each book will have 2 files attachment, image and resource.
+  validates_attachment :image, presence: true ,
+  content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] },
+  message: 'Only Images allowed'
+
+  validates_attachment :resource, presence: true ,
+  content_type: { content_type: "application/pdf" },
+  message: 'Only PDF allowed '
+
 end
